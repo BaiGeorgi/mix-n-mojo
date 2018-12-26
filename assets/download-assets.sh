c919057af2326c852081c000f1da2ee13d973e0d
Георги Пацев <georgi.patsev@gmail.com>
2018-12-26 18:18:47 +0200

    Update download-assets.sh

--- assets/download-assets.sh
+++ assets/download-assets.sh
@@ -1,2 +1,2 @@
-wget http://www.oldgames.sk/docs/Dial-A-Pirate/dialapirate-c1.png -O front.png
-wget http://www.oldgames.sk/docs/Dial-A-Pirate/dialapirate-c2.png -O back.png
+wget http://www.oldgames.sk//images/codewheel/monkey-island-2/mixnmojo-w2.png -O front.png
+wget http://www.oldgames.sk//images/codewheel/monkey-island-2/mixnmojo-w1.png -O back.png
